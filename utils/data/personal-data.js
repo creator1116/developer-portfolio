8e1bd36524139a16332fca3915013df55dd00723
Dragon Fire <133981966+creator1116@users.noreply.github.com>
2024-04-30 15:07:03 -0700

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -4,7 +4,7 @@ export const personalData = {
   designation: "Software Developer",
   description: "My name is ABU SAID. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'Maiguin@outlook.com',
-  phone: '',
+  phone: '(111)-111-1111',
   address: 'Redmond Washington,
   github: 'https://github.com/creator1116',
   facebook: 'https://www.facebook.com/abusaid.riyaz/',
